33050ab26f5ad8e7f0f95e19ad3e02b4582571b7
h3d-longnsp <hydroxyzine@metacity.email>
2023-03-18 20:42:30 +0700

    Add arguments parser.

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 - Get `requirements.txt`: `python3 -m  pipreqs.pipreqs --encoding utf-8 .`
 - Install requirements: `pip install -r requirements.txt`
 - test get baomoi urls: `python3 test_url.py > test_url.txt`
+- Command-line arguments:
+```
+    usage: app.py [-h] [-c CATEGORY]
+
+    Python web scrapper.
+
+    options:
+    -h, --help            show this help message and exit
+    -c CATEGORY, --category CATEGORY
+                            Category to scrape.
+```
+- List of categories: `the-gioi, xa-hoi, van-hoa, kinh-te, giao-duc, the-thao, giai-tri, phap-luat, khoa-hoc-cong-nghe, khoa-hoc, doi-song, xe-co, nha-dat`
--- app.py
+++ app.py
@@ -1,25 +1,34 @@
 import utils 
+import argparse
 
 URL_REGEX = r'href="(\/\S*?.epi)"'
-CATEGORY = "https://baomoi.com/khoa-hoc/trang1.epi/"
 
 posts_date       = []
 posts_content    = []
 posts_unique_url = []
 
-pagination = list(range(1, 10))
+def parser():
+    parser = argparse.ArgumentParser(description='Python web scrapper.')
+    parser.add_argument("-c", "--category", help="Category to scrape.", default="khoa-hoc.")
+    return parser.parse_args()    
 
-html = utils.getPageResponse(CATEGORY)
-posts_url = utils.getURLFromPage(html, URL_REGEX)
+def main():
+    args = parser()
+    category_url = f"https://baomoi.com/{args.category}.epi"
+    html = utils.getPageResponse(category_url)
+    posts_url = utils.getURLFromPage(html, URL_REGEX)
 
-for url in posts_url:
-    body, date = utils.getPageContent(url)
-    posts_content.append(body)
-    posts_date.append(date)
+    for url in posts_url:
+        body, date = utils.getPageContent(url)
+        posts_content.append(body)
+        posts_date.append(date)
 
-for content in posts_content:
-    posts_unique_url.append(utils.urlRegex(content))
+    for content in posts_content:
+        posts_unique_url.append(utils.urlRegex(content))
 
-jsonData = utils.jsonFormatter(posts_unique_url, posts_content, posts_date)
+    jsonData = utils.jsonFormatter(posts_unique_url, posts_content, posts_date)
 
-utils.writeFile("NguyenSyPhiLong.json", jsonData)
+    utils.writeFile("NguyenSyPhiLong.json", jsonData) 
+
+if __name__ == "__main__":
+    main()
